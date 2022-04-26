2b41b50d688d634052e334b0b884932bbea4d7f8
Carlos Vargas Huaman <carlos.vargas.huaman@avantica.net>
2022-04-25 20:52:12 -0500

    New main menu added

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,38 @@
+import presentation.MenuPresenter
+import util.MENU_EMPLOYEE
+import util.MENU_EXIT
+import util.MENU_ORDERS
+import util.MENU_SALES
+
 fun main(array: Array<String>) {
-    println("hola")
+
+    // Presenters
+    val menuPresenter = MenuPresenter()
+
+    var mainManuOption = 0
+    var runMainProgram = true
+
+    while (runMainProgram) {
+
+        menuPresenter.showMainTitle()
+        mainManuOption = readLine()!!.toInt()
+
+        when (mainManuOption) {
+            MENU_SALES -> {
+
+            }
+            MENU_EMPLOYEE -> {
+
+            }
+            MENU_ORDERS -> {
+
+            }
+            MENU_EXIT -> {
+                runMainProgram = false
+            }
+
+        }
+
+    }
+
 }
